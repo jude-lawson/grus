0af3c5c4cbb72a0eba4261523bf74f3b40e7cde5
Jude <lawsonjdutton@gmail.com>
2019-11-24 11:43:28 -0700

    Added in facotry for tasks and updated user factory to sequence username values:

--- grus_be/spec/factories/task_factory.rb
+++ grus_be/spec/factories/task_factory.rb
@@ -1,6 +1,13 @@
 require 'faker'
 
+stuff = %w(some most all none)
+
 FactoryBot.define do
   factory :task do
-    name { }
+    name { "Do #{stuff.sample} of the things" }
+    description { "This is a sample task used for development" }
+    created_by { "Tester" }
+    modified_by { "Tester" }
+    user
+  end
 end
--- grus_be/spec/factories/user_factory.rb
+++ grus_be/spec/factories/user_factory.rb
@@ -1,10 +1,11 @@
 require 'faker'
 
-FactoryBot.define do
+FactoryBot.define do 
   factory :user do
     first_name { Faker::Name.first_name }
     middle_name { Faker::Name.middle_name }
     last_name { Faker::Name.last_name }
+    sequence(:username) { |n| "#{first_name}.#{last_name}_#{n}" }
     motto { Faker::Quotes::Shakespeare.hamlet }
     created_by { "Tester" }
     modified_by { "Tester" }
