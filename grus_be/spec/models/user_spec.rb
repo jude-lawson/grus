304411354adde35cb663f907c91be92d6ba58a61
Jude <lawsonjdutton@gmail.com>
2019-11-24 15:02:21 -0700

    Configured shoulda-matchers

--- grus_be/spec/models/user_spec.rb
+++ grus_be/spec/models/user_spec.rb
@@ -1,9 +1,5 @@
 require_relative '../rails_helper'
 
 RSpec.describe 'User' do
-  context 'default test' do
-    it 'should tell the truth' do
-      expect(User.tell_the_truth).to eq true
-    end
-  end
+  describe ""
 end
