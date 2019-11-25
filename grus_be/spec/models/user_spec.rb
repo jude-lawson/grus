4b1f4b3b36f989d6337da7927841b43438131436
jude <lawsonjdutton@gmail.com>
2019-11-24 22:48:09 -0700

    Tested user tasks association and unique usernames

--- grus_be/spec/models/user_spec.rb
+++ grus_be/spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require_relative '../rails_helper'
 
-RSpec.describe 'User' do
-  describe ""
+RSpec.describe User, type: :model do
+  describe 'Associations' do
+    it { should have_many :tasks }
+
+    it 'should only allow unique usernames' do
+      create(:user, username: "originally_unique")
+
+      expect do 
+        create(:user, username: "originally_unique") 
+      end.to raise_error(ActiveRecord::RecordNotUnique)
+    end
+  end
 end
